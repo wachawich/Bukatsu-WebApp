9b15c6c089b78507aa5180bee85ddb4c2d785889
Xenoszz <Natthinan.safety@gmail.com>
2025-05-01 22:15:45 +0700

    first commit

---
+++ src/pages/clubdetail/index.tsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react'
+
+import LayoutShell from "@/comps/layouts/LayoutShell";
+
+import Homepage from "@/comps/homepage/home"
+
+import { getUser } from "@/utils/api/userData"
+import Clubdetail from '@/comps/clubdetailpage/clubdetail';
+
+function PageContent() {
+    // const [canRead, setCanRead] = useState(false);
+
+    // useCheckTokenFlags(
+    //     [{ flag: "read", onFlagUpdated: setCanRead }],
+    //     "check_in"
+    // );
+
+    return (
+        <div className="bg-[#343434] w-full h-full pt-14">
+            <Clubdetail/>
+        </div>
+        // <>
+        //     {canRead && (
+        //         <div className="fadeIn-animation">
+        //             {/* <CheckIn /> */}
+        //         </div>
+        //     )}
+        // </>
+    );
+}
+
+
+export default function Club() {
+    return (
+        <>
+            <LayoutShell>
+                <PageContent></PageContent>
+            </LayoutShell>
+        </>
+    );
+}
